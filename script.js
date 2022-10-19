8eeeec21f7095774d9aabf5b965c3c05a6b3fa23
sleman.khald <109400695+slemankhald@users.noreply.github.com>
2022-10-19 14:09:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const span = document.getElementById("multi-words")
+
+const words = ["Code it", "Rebuild it", "Update it", "Beautify it"]
+
+
+
+
+
+
+setTimeout(
+  setInterval(function () {
+    span.innerText = words[Math.floor(Math.random()*words.length)]
+  }, 1500)
+  , 2000
+);
+
+$(document).ready(function(){
+  var wrench = $("#wrench");
+  var nut = $("#nut");
+
+  var tm = new TimelineMax({repeat: -1});
+
+    tm
+      .to(wrench, 0.3, {y: 38})
+      .to(wrench, 0.4, {rotation: 120, transformOrigin: '49% 95%'})
+      .to(nut, 0.4, {rotation: 120, transformOrigin: '50% 50%'}, '-=0.4')
+      .to(wrench, 0.4, {x: 38, y: 55})
+      .to(wrench, 0.4, {y:0, x: 0, rotation: 0})
+})
